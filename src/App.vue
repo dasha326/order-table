f1b379478058dc52a82e1bab70f05f9b776fc45d
Dasha <dasha@100up.ru>
2023-02-22 15:41:53 +0300

    start websocket

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+        <input type="text" v-model="message">
+        <button @click="sendMessage()">Отправить</button>
+    </label>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    import PieSocket from 'piesocket-js';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        data() {
+            return {
+                channelPieSocket: null,
+                message: ''
+            }
+        },
+        methods: {
+            sendMessage() {
+                return this.channelPieSocket.publish("new_message", {
+                    message: this.message
+                })
+            }
+        },
+        mounted() {
+            const pieSocket = new PieSocket({
+                clusterId: "demo",
+                apiKey: "BidybaY8rstaLmPA54AHPhOkmKFFExpuOE9ol6qa",
+                notifySelf: true
+            });
+
+            pieSocket.subscribe("Channel-order-table").then((channel) => {
+                console.log("Channel is ready");
+                this.channelPieSocket = channel;
+            });
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
